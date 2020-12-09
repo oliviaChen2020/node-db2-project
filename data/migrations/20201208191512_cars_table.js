b13fa1d5fd4f3b5ddedc8b79c6b88819b82bc7a5
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-12-08 19:32:05 -0500

    mistakes in migration file corrected

--- data/migrations/20201208191512_cars_table.js
+++ data/migrations/20201208191512_cars_table.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('fruits', (table) => {
+  return knex.schema.createTable('cars', (table) => {
     // id: primary key, unique, not null (integers that auto-increment)
     table.increments();
     table.text('VIN', 128).unique().notNullable();
     table.text('make', 128).notNullable();
-    table.text('mode', 128).unique().notNullable();
-    table.text('mileage').unique().notNullable();
+    table.text('mode', 128).notNullable();
+    table.text('mileage').notNullable();
   });
 };
 
